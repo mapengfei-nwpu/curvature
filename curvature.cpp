3dec6c753600ddd2e305971d311abf8929a1e7dd
mapengfei-nwpu <33214870+mapengfei-nwpu@users.noreply.github.com>
2019-08-17 12:07:21 +0800

    redesigned variables.

--- curvature.cpp
+++ curvature.cpp
@@ -1,38 +1,51 @@
-namespace closed_curve{
-    class Point{
-        public:
-        Point(double x, double y, double z);
-        private:
-        double x[3];
-    }
-    class Triangle{
-        public:
-        Triangle(Point a, Points b, Point c);
-        std::shared_ptr<size_t> index() const;
-        std::shared_ptr<double> normal() const;
-        private:
-        double normal[3];
-        Point points[3];
-        size_t index[3];
-    }
-    class Ring{
-        public:
-        Ring(Point center, vector<Point> edge);
+#include<iostream>
+#include<vector>
 
-        private:
-        Point center;
-        double normal[3];
-        double curvature;
+namespace pengfei {
+	class Point {
+	public:
+		Point(double a, double b, double c) :x{ a, b, c } {};
+	private:
+		double x[3];
+	};
+	class Triangle {
+	public:
+		Triangle(Point a, Point b, Point c) :points{ a,b,c },_normal(new double[3]) {
+			// 调用一个计算normal的函数
+		};
+		std::shared_ptr<double> normal() const {
+			return _normal;
+		};
+	private:
+		std::shared_ptr<double> _normal;
+		Point points[3];
+	};
+	class Ring {
+	public:
+		Ring(Point center, std::vector<Point> edge) :
+			_center(center),
+			_edge(edge),
+			_normal(new double[3]) {
+			//把边上的点和
+		};
+	private:
+		Point _center;
+		std::vector<Point> _edge;
+		std::vector<Triangle> _triangles;
+		std::shared_ptr<double> _normal;
+		double curvature;
+	};
+	/*class Curve {
+	public:
+		Curve(std::vector<Point> points, std::vector<Triangle> triangles);
+	private:
+		std::vector<Point> point;
+		std::vector<size_t> triangles;
+		std::vector<Ring> rings;
 
-
-    }
-    class Curve{
-        public:
-        Curve(vector<Point> points, vector<Triangle> triangles);
-        private:
-        vector<Point> point;
-        vector<size_t> triangles;
-        vector<Ring> rings;
-
-    }
+	};*/
+}
+int main() {
+	pengfei::Point p(1, 1, 1);
+	std::cout << "Hello world!" << std::endl;
 }
